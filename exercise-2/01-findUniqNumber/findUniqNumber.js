251e34f206d934d7e5b03dcecc10a62b2e46275b
Damir Shagaev <shagaevdr@gmail.com>
2022-10-20 05:27:20 +0300

    оформление заданий

---
+++ exercise-2/01-findUniqNumber/findUniqNumber.js
@@ -0,0 +1,11 @@
+/**
+ * Описание: найти уникальное число в массиве чисел
+ * Ожидаемый результат: findUniqNumber([ 1, 1, 1, 2, 1, 1 ]) => 2;
+ * @param {Array<number>} arr - Array
+ * @returns {Number}
+ */
+const findUniqNumber = arr => {
+  
+}
+
+module.exports = findUniqNumber;
---
+++ exercise-2/02-findUniqArray/findUniqArray.js
@@ -0,0 +1,11 @@
+/**
+ * Описание: найти уникальные числа в массиве чисел
+ * Ожидаемый результат: findUniqArray([1, 1, 2, 2, 4, 2, 3, 7, 3]) => [1, 2, 4, 3, 7];
+ * @param {Array<number>} arr - Array
+ * @returns {Array<number>}
+ */
+const findUniqArray = arr => {
+  
+}
+
+module.exports = findUniqArray;
