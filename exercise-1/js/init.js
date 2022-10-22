3c7cc77eaf907c12d733ebbbbb6d1ab7261abd37
Alexander <kaamosdao@gmail.com>
2022-10-22 21:10:27 +0300

    add errors handling

--- exercise-1/js/init.js
+++ exercise-1/js/init.js
@@ -2,17 +2,27 @@ import '../css/main.scss';
 import validate from './validate';
 
 export default () => {
-  const form = document.querySelector('.form');
-  form.addEventListener('submit', (e) => {
+  const elements = {
+    form: document.querySelector('.form'),
+    emailAlert: document.querySelector('.input-email__alert'),
+    passwordAlert: document.querySelector('.input-password__alert'),
+    passwordInput: document.querySelector('.input-password__input'),
+    emailInput: document.querySelector('.input-email__input'),
+  };
+
+  elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    // console.log(formData.get('email'));
-    // console.log(formData.get('password'));
-    // console.log(formData.get('checkboxRemember'));
+
     try {
       const email = formData.get('email');
       const password = formData.get('password');
       const shouldRemember = !!formData.get('checkboxRemember');
+
+      elements.emailAlert.classList.add('hide');
+      elements.passwordInput.classList.remove('input-password__input--invalid');
+      elements.passwordAlert.classList.add('hide');
+      elements.emailInput.classList.remove('input-email__input--invalid');
 
       validate(email, password);
 
@@ -22,7 +32,18 @@ export default () => {
         console.log('no');
       }
     } catch (error) {
-      error.errors.map((e) => console.log(e.field));
+      error.errors.forEach((error) => {
+        if (error.field === 'email') {
+          elements.emailAlert.textContent = error.message;
+          elements.emailAlert.classList.remove('hide');
+          elements.emailInput.classList.add('input-email__input--invalid');
+        }
+        if (error.field === 'password') {
+          elements.passwordAlert.textContent = error.message;
+          elements.passwordAlert.classList.remove('hide');
+          elements.passwordInput.classList.add('input-password__input--invalid');
+        }
+      });
     }
   });
 };
